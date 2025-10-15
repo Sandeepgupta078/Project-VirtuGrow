7f8939ed862ba15c179d401aecf879b62d4d271b
Sandeepgupta078 <sandeepmgs078@gmail.com>
2025-10-15 10:11:41 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://project-virtugrow-api.onrender.com/' + '/api', // backend url
+  baseURL: import.meta.env.VITE_API_URL + "/api", // backend url
   headers: {
     'Content-Type': 'application/json',
   },
