b7b49d5a30a9e6a04086baa6ee1fcbf3c43a53bb
Sandeepgupta078 <sandeepmgs078@gmail.com>
2025-10-15 09:49:23 +0530

    updated backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api', // backend url
+  baseURL: 'https://project-virtugrow-api.onrender.com', // backend url
 });
 
 // Add token to each request
